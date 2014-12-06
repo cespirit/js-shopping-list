05ff5ffda94496a7235ddf415f4fe1299aa8e64e
cespirit <ckespir@gmail.com>
2014-12-05 21:34:35 -0800

    Function check and uncheck feature. Press enter also adds new item.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,45 @@
  $(document).ready(function(){
 
- 	$(".add-item").on("click", ".add",  function(){
- 		var $item = $("#new-item").val();
- 		$item = $.trim($item); 
- 		if($item === "") {
- 			alert("Please type in an item to add.");
- 			return;
- 		}
+ 	/* Add item to list*/
+ 	$(".add-item").on("click", ".add",  addItem);
 
- 		var $newItem = $('<li class="item"><input type="checkbox">' + 
- 			            '<button class="delete fa fa-times"></button>' 
- 			            + $item + "</li>");
- 		$("#items-list").append($newItem);
+ 	$("#new-item").on('keyup', function(e){
+ 		if(e.keyCode === 13){
+ 			addItem();
+ 		}
  	});
 
+ 	/* Move items between completed and not completed list */
+ 	$(".lists").on("click", "input[type='checkbox']", function(){
+ 		var $item = $(this).closest(".item");
+ 		
+ 		if( $(this).prop("checked") ) {
+ 			$item.css({"text-decoration":"line-through", 
+ 				       "background-color":"#d8d8d8"});
+ 			$("#completed-list").append( $item ); 			
+ 		} else {
+ 			$item.css({"text-decoration":"none", "background-color":""});
+ 			$("#items-list").append( $item ); 
+ 		}
+ 	});
+
+ 	/* Delete item from list */
  	$(".lists").on("click", ".delete", function(){
  		$(this).closest(".item").remove();
  	});
 
  });
+
+function addItem() {
+	var $item = $("#new-item").val();
+	$item = $.trim($item); 
+	if($item === "") {
+		alert("Please type in an item to add.");
+		return;
+	}
+
+	var $newItem = $('<li class="item"><input type="checkbox">' + 
+		            '<button class="delete fa fa-times"></button>' 
+		            + $item + "</li>");
+	$("#items-list").append($newItem);
+}
